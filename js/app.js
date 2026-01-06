e0919b4f77ed844a25745d9a9f542a396e5e587f
PGabarini <pagabarini@frba.utn.edu.ar>
2026-01-06 19:27:25 -0300

    Agrego modales

--- js/app.js
+++ js/app.js
@@ -17,4 +17,62 @@ document.addEventListener('DOMContentLoaded',()=>{
 
         })
     })
+
+    /*Cerrar modales */
+    const modales = Array.from(document.querySelectorAll('.modal'))
+
+    modales.forEach( m => {
+
+        m.addEventListener(('click'), (e) => {
+          
+            if(e.target === m){
+                m.style.display ='none'
+            }
+            
+        })
+    })
+
+    /*Abrir modal metamapa */
+    const verMasMetamapa = document.getElementById('detalle-metamapa')
+
+    verMasMetamapa.addEventListener(('click'),() => {
+        const modalMetamapa = document.getElementById('modal-metamapa')
+        modalMetamapa.style.display = 'flex'
+    })
+
+    /*Abrir modal SO */ 
+    const verSo = document.getElementById('detalle-so')
+
+    verSo.addEventListener(('click'),() => {
+        const so = document.getElementById('modal-so')
+        so.style.display = 'flex'
+    })
+
+    /*Abrir modal Tienda Sol */
+    const verSol = document.getElementById('detalle-sol')
+
+    verSol.addEventListener(('click'),() => {
+        const sol = document.getElementById('modal-sol')
+        sol.style.display = 'flex'
+    })
+
+
+
+    /*Abrir modal sql */
+    const verSql = document.getElementById('detalle-sql')
+
+    verMasMverSqletamapa.addEventListener(('click'),() => {
+        const sql = document.getElementById('modal-sql')
+        sql.style.display = 'flex'
+    })
+
+    /*Abrir modal micro */
+    const verMicro = document.getElementById('detalle-micro')
+
+    verMicro.addEventListener(('click'),() => {
+        const micro = document.getElementById('modal-micro')
+        micro.style.display = 'flex'
+    })
+
+
 })
